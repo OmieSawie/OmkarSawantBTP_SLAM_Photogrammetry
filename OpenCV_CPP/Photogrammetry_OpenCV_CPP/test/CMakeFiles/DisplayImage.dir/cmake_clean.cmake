dd42a8247c72f71921da092f4f05f696b0535038
Omie Sawie <omkarsawant20HG@gmail.com>
2023-05-15 10:13:47 +0530

    First Commit

---
+++ OpenCV_CPP/Photogrammetry_OpenCV_CPP/test/CMakeFiles/DisplayImage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DisplayImage.dir/FaceDetection.cpp.o"
+  "CMakeFiles/DisplayImage.dir/FaceDetection.cpp.o.d"
+  "DisplayImage"
+  "DisplayImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DisplayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
