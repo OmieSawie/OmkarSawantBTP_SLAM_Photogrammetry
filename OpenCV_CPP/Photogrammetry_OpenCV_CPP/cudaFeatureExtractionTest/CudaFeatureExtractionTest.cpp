caa7af83c6e1374cc2e7833c43a02fff98e8c40e
Omie Sawie <omkarsawant20HG@gmail.com>
2023-12-11 01:56:53 +0530

    Essential Matrix and Epilines computation

--- OpenCV_CPP/Photogrammetry_OpenCV_CPP/cudaFeatureExtractionTest/CudaFeatureExtractionTest.cpp
+++ OpenCV_CPP/Photogrammetry_OpenCV_CPP/cudaFeatureExtractionTest/CudaFeatureExtractionTest.cpp
@@ -6,11 +6,14 @@
 #include <opencv2/core/cvstd_wrapper.hpp>
 #include <opencv2/core/hal/interface.h>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/core/matx.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
 #include <streambuf>
 #include <unistd.h>
 #include <vector>
@@ -52,6 +55,7 @@ class FeatureExtractor {
     void extractFeatures_goodFeaturesToTrack(int, void *) {
 
         cv::cuda::GpuMat src_gray_gpu = cv::cuda::GpuMat(src_gray);
+        cv::cuda::GpuMat prev_src_gray_gpu = cv::cuda::GpuMat(prev_src_gray);
 
         if (!prev_src_gray_gpu.empty()) {
             cout << " Size of image:" << prev_src_gray_gpu.size() << endl;
@@ -85,7 +89,7 @@ class FeatureExtractor {
             std::vector<cv::DMatch> good_matches;
             for (int k = 0;
                  k < min(descriptors_gpu.rows - 1, (int)matches.size()); k++) {
-                if ((matches[k][0].distance < 0.8 * (matches[k][1].distance))) {
+                if ((matches[k][0].distance < 0.9 * (matches[k][1].distance))) {
                     good_matches.push_back(matches[k][0]);
                     /* cout << matches[k][1].queryIdx << " " */
                     /*      << matches[k][1].trainIdx << " "; */
@@ -103,39 +107,84 @@ class FeatureExtractor {
             vector<cv::KeyPoint> keypoints_cpu, prev_keypoints_cpu;
             extractor->convert(keypoints_gpu, keypoints_cpu);
             extractor->convert(prev_keypoints_gpu, prev_keypoints_cpu);
-            cout << "Keypoints Size :" << keypoints_cpu[100].size << endl;
+            cout << "Keypoints Size :" << keypoints_cpu.size() << endl;
 
             // Draw the Keypoints on the source gray image
             drawKeypoints(src_gray, keypoints_cpu, src_gray,
-                          cv::Scalar(255, 0, 0),
+                          cv::Scalar(255, 0, 255),
                           cv::DrawMatchesFlags::DRAW_OVER_OUTIMG);
-            cv::imshow("Kps", src_gray);
+            /* cv::imshow("Kps", src_gray); */
 
             // Draw the Matches
             cv::Mat img_matches;
-            drawMatches(src, keypoints_cpu, prev_src_gray, prev_keypoints_cpu,
-                        good_matches, img_matches);
-            cv::namedWindow("matches", 0);
-            imshow("matches", img_matches);
+            /* drawMatches(prev_src_gray, prev_keypoints_cpu, src_gray, */
+            /*             keypoints_cpu, good_matches, img_matches); */
+            /* cv::namedWindow("matches", 0); */
+            /* imshow("matches", img_matches); */
 
             // Estimating the Fundamental Matrix
-            int point_count = 100;
+            int point_count = good_matches.size();
             vector<cv::Point2f> points1(point_count);
             vector<cv::Point2f> points2(point_count);
             // initialize the points here ...
             for (int i = 0; i < point_count; i++) {
-                points1[i] = keypoints_cpu[i].pt;
-                points2[i] = prev_keypoints_cpu[i].pt;
+                points1[i] = prev_keypoints_cpu[good_matches[i].trainIdx].pt;
+                points2[i] = keypoints_cpu[good_matches[i].queryIdx].pt;
             }
-            cout << cv::findFundamentalMat(points1, points2, cv::FM_RANSAC, 3,
-                                           0.95)
-                 << endl;
+            cv::Mat fundamentalMatrix = cv::findFundamentalMat(
+                points1, points2, cv::FM_RANSAC, 3, 0.95);
+            /*  cout << fundamentalMatrix << endl; */
+
+            // Estimating the Essential Matrix
+            cv::Mat cameraMatrix = (cv::Mat_<float>(3, 3) << 2422, 0, 2138, 0,
+                                    2430, 1137, 0, 0, 1);
+            cv::Mat essentialMatrix;
+            essentialMatrix = cv::findEssentialMat(
+                points1, points2, cameraMatrix, cv::RANSAC, 0.999, 1.0, 1000);
+            /* cout << essentialMatrix << endl; */
+            cv::Mat R1, R2, t;
+            cv::decomposeEssentialMat(essentialMatrix, R1, R2, t);
+            cout << R1 << endl << R2 << endl << t << endl;
+
+            // Compute epilines
+            vector<cv::Vec3f> epilines1, epilines2;
+            cv::computeCorrespondEpilines(points1, 1, fundamentalMatrix,
+                                          epilines1);
+            cv::computeCorrespondEpilines(points2, 2, fundamentalMatrix,
+                                          epilines2);
+            /* cout << epilines << endl; */
+            for (vector<cv::Vec3f>::const_iterator it = epilines1.begin();
+                 it != epilines1.end(); ++it) {
+                // draw the line between first and last column
+                cv::line(
+                    prev_src_gray, cv::Point(0, -(*it)[2] / (*it)[1]),
+                    cv::Point(4624, -((*it)[2] + (*it)[0] * 4624) / ((*it)[1])),
+                    cv::Scalar(255, 255, 255));
+            }
+            for (vector<cv::Vec3f>::const_iterator it = epilines2.begin();
+                 it != epilines2.end(); ++it) {
+                // draw the line between first and last column
+                cv::line(
+                    src_gray, cv::Point(0, -(*it)[2] / (*it)[1]),
+                    cv::Point(4624, -((*it)[2] + (*it)[0] * 4624) / ((*it)[1])),
+                    cv::Scalar(255, 255, 255));
+            }
+
+            /* cv::imshow("Right Image Epilines (FM_7POINT2)", src_gray); */
+            /* cv::imshow("Right Image Epilines (FM_7POINT1)",
+             * prev_src_gray);
+             */
+
+            drawMatches(src_gray, keypoints_cpu, prev_src_gray,
+                        prev_keypoints_cpu, good_matches, img_matches);
+            cv::namedWindow("matches", 0);
+            imshow("matches", img_matches);
 
         } // End of if statement
 
         src_gray_gpu.copyTo(prev_src_gray_gpu);
         src_gray.copyTo(prev_src_gray);
-        cout << "Size 1" << prev_src_gray.size() << endl;
+        /* cout << "Size 1" << prev_src_gray.size() << endl; */
     }
 
     void estimateFundamentalMatrix() {}
@@ -148,62 +197,72 @@ int main() {
     /* cv::cuda::printCudaDeviceInfo(0); */
 
     // initialize a video capture object
-    cv::VideoCapture vid_capture(
-        "/home/omie_sawie/Code_Code/OmkarSawantBTP_SLAM_Photogrammetry/"
+    /* cv::VideoCapture vid_capture( */
+    /*     "/home/omie_sawie/Code_Code/OmkarSawantBTP_SLAM_Photogrammetry/"
+     */
 
-        "OpenCV_CPP/Photogrammetry_OpenCV_CPP/videoFeatureMatching/"
+    /*     "OpenCV_CPP/Photogrammetry_OpenCV_CPP/videoFeatureMatching/" */
 
-        "/resources/carOnLonelyRoads.mp4");
+    /*     "/resources/carOnLonelyRoads.mp4"); */
+
+    /* cv::VideoCapture vid_capture("../testVideo1.mp4"); */
 
     // Print error message if the stream is invalid */
-    if (!vid_capture.isOpened()) {
-        cout << "Error opening video stream or file" << endl;
-    }
+    /* if (!vid_capture.isOpened()) { */
+    /*     cout << "Error opening video stream or file" << endl; */
+    /* } */
 
-    while (vid_capture.isOpened()) {
-        // Initialise frame matrix
-        // Initialize a boolean to check if frames are there or not
-        cv::Mat frame;
-        bool isSuccess = vid_capture.read(frame);
+    /* while (vid_capture.isOpened()) { */
+    // Initialise frame matrix
+    // Initialize a boolean to check if frames are there or not
+    /* cv::Mat frame; */
+    /* bool isSuccess = vid_capture.read(frame); */
 
-        src = frame.clone();
+    /* src = frame.clone(); */
 
-        cv::cvtColor(src, src_gray, cv::COLOR_RGB2GRAY);
+    src_gray = cv::imread("../testimg1.jpg", cv::IMREAD_GRAYSCALE);
+    prev_src_gray = cv::imread("../testimg2.jpg", cv::IMREAD_GRAYSCALE);
 
-        /* cv::setTrackbarPos("Max corners", source_window, maxCorners); */
+    /* cv::cvtColor(src, src_gray, cv::COLOR_RGB2GRAY); */
 
-        /* imshow(source_window, src); */
+    /* cv::setTrackbarPos("Max corners", source_window, maxCorners); */
 
-        FeatureExtractor feature_extractor;
+    /* imshow(source_window, src); */
 
-        feature_extractor.extractFeatures_goodFeaturesToTrack(0, 0);
+    FeatureExtractor feature_extractor;
 
-        // If frames are present, show it
-        if (isSuccess == true) {
-            // display frames
-            /* imshow("Frame", frame); */
-            /* imshow("PrevFrame", prevFrame); */
-        }
+    feature_extractor.extractFeatures_goodFeaturesToTrack(0, 0);
 
-        if (isSuccess == false) {
-            cout << "Video camera is disconnected" << endl;
-            break;
-        }
+    /* // If frames are present, show it */
+    /* if (isSuccess == true) { */
+    /*     // display frames */
+    /*     /1* imshow("Frame", frame); *1/ */
+    /*     /1* imshow("PrevFrame", prevFrame); *1/ */
+    /* } */
 
-        //-- Step 1: Detect the keypoints using SURF Detector,compute
-        // the
-        // descriptors
+    /* if (isSuccess == false) { */
+    /*     cout << "Video camera is disconnected" << endl; */
+    /*     break; */
+    /* } */
 
-        // wait 20 ms between successive frames and break the loop ifkey
-        // q is pressed
-        int key = cv::waitKey(1000);
+    //-- Step 1: Detect the keypoints using SURF Detector,compute
+    // the
+    // descriptors
+
+    // wait 20 ms between successive frames and break the loop ifkey
+    // q is pressed
+    while (true) {
+        int key = cv::waitKey();
         if (key == 'q') {
-            cout << "q key is pressed by the user. Stopping the video" << endl;
+            cout << "q key is pressed by the user. Stopping the "
+                    "video"
+                 << endl;
             break;
         }
     }
+    /* } */
     // Release the video capture object
-    vid_capture.release();
+    /* vid_capture.release(); */
     cv::destroyAllWindows();
     return 0;
 }
